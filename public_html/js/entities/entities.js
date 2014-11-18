7b69c75658030e5e9ea29a66cae09d23ba692660
antonio hernandez <defygaming99@gmail.com>
2014-11-18 11:25:41 -0800

    more stuff

--- public_html/js/entities/entities.js
+++ public_html/js/entities/entities.js
@@ -12,10 +12,10 @@ game.PlayerEntity = me.Entity.extend({
         } 
     }]); 
 
-//        this.renderable.addAnimation("idle", [3]);
-//        this.renderable.addAnimation("smallWalk", [8, 9, 10, 11, 12, 13], 80);
-//        
-//        this.renderable.setCurrentAnimation("idle"); 
+        this.renderable.addAnimation("idle", [3]);
+        this.renderable.addAnimation("smallWalk", [8, 9, 10, 11, 12, 13], 80);
+        
+        this.renderable.setCurrentAnimation("idle"); 
         
         this.body.setVelocity(5, 20);
         
@@ -23,21 +23,21 @@ game.PlayerEntity = me.Entity.extend({
     },
     
     update: function(delta) {
-//        if(me.input.isKeyPressed("right")) {
-//            this.body.vel.x += this.body.accel.x * me.timer.tick; 
-//            
-//        }else{
-//            this.body.vel.x = 0;
-//        }
-//        
-//        if (this.body.vel.x !== 0) {
-//            if (!this.renderable.isCurrentAnimation("smallWalk")); {
-//                this.renderable.setCurrentAnimation("smallWalk");
-//                this.renderable.setAnimationFrame(); 
-//            }
-//        }else{
-//            this.renderable.setCurrentAnimation("idle"); 
-//        }
+        if(me.input.isKeyPressed("right")) {
+            this.body.vel.x += this.body.accel.x * me.timer.tick; 
+            
+        }else{
+            this.body.vel.x = 0;
+        }
+        
+        if(this.body.vel.x !== 0) {
+             if(!this.renderable.isCurrentAnimation("smallWalk")) {
+                this.renderable.setCurrentAnimation("smallWalk");
+                this.renderable.setAnimationFrame(); 
+            }
+        }else{
+            this.renderable.setCurrentAnimation("idle"); 
+        }
         this.body.update(delta);
         this._super(me.Entity, "update", [delta]);
         return true; 
