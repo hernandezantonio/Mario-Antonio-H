09518f056704c8c9df55e41132245aaed162a809
antonio hernandez <defygaming99@gmail.com>
2014-12-07 22:27:58 -0800

    bad guy done

--- public_html/js/entities/entities.js
+++ public_html/js/entities/entities.js
@@ -66,3 +66,63 @@ game.LevelTrigger = me.Entity.extend ({
     }
     
 }); 
+
+game.BadGuy = me.Entity.extend({
+    init: function(x, y, settings){
+        this._super(me.Entity, 'init', [x, y, {
+             image:"slime", 
+             spritewidth:"60",
+             spriteheight: "28",
+             width: 60,   
+             height: 28,
+             getShape: function (){
+                 return (new me.Rect(0, 0, 00, 28)).toPolygon();
+             }
+         }]);
+     
+     this.spritewidth = 60;
+     var width = settings.width;
+     x = this.pos.x;
+     this.startX = x;
+     this.endX = x + width - this.spritewidth;
+     this.pos.x = x + width -this.spritewidth; 
+     this.updateBounds();
+     
+     this.alwaysUpdate = true;
+     
+     this.walkLeft = false;
+     this.alive = true;
+     this.type = "badguy"; 
+     
+     
+     
+     this.body.setVelocity(4, 6); 
+}, 
+
+update: function(delta){
+    this.body.update(delta);
+    me.collision.check(this, true, this.collideHandler.bind(this), true); 
+    
+    if(this.alive){
+        if(this.walkLeft && this.pos.x <= this.startX){
+            this.walkLeft = false;
+        }else if(!this.walkleft && this.pos.x >= this.endX){
+            this.walkLeft = true;
+        }
+        this.flipX(!this.walkLeft);
+        this.body.vel.x += (this.walkLeft) ? -this.body.accel.x * me.timer.tick : this.body.accel.x * me.timer.tick;
+        
+    }else{
+        me.game.world.removeChild(this);
+    }
+    
+    
+    this._super(me.Entity, "update", [delta]);
+    return true; 
+    
+   },
+   
+   collideHandler: function(){
+       
+   }
+}); 
